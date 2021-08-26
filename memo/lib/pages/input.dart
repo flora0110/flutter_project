a40548e1f90f35b1648f0bd0acdbfdbc0c27f626
flora0110 <58814336+flora0110@users.noreply.github.com>
2021-08-26 08:11:53 +0800

    input page upgrade

--- memo/lib/pages/input.dart
+++ memo/lib/pages/input.dart
@@ -14,23 +14,40 @@ class _InputState extends State<Input> {
   Widget build(BuildContext context) {
     print('input build');
     return Scaffold(
+      backgroundColor: Colors.blue[50],
         appBar: AppBar(
-          title: const Text('input'),
+          title: const Text('add new quote'),
           centerTitle: true,
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
             children: <Widget>[
               TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: "text:",
+                  labelStyle: TextStyle(fontSize: 25,),
+                  hintText: "please input the text of quote",
+                ),
                 controller: textController,
-                decoration: InputDecoration(hintText: '請輸入內容'),
+                //decoration: InputDecoration(hintText: '請輸入內容'),
               ),
               TextField(
+                decoration: InputDecoration(
+                  labelText: "author:",
+                  labelStyle: TextStyle(fontSize: 25,),
+                  hintText: "please author the text of quote",
+                ),
                 controller: authorController,
-                decoration: InputDecoration(hintText: '請輸入作者'),
+                //decoration: InputDecoration(hintText: '請輸入作者'),
               ),
               ElevatedButton(
-                child: Text('ok'),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(100,40),
+                ),
+                child: Text('ok', style: TextStyle(
+                  fontSize: 20,)),
                 //onPressed: btnEvent,
                 onPressed: (){
                   print('press ok');
